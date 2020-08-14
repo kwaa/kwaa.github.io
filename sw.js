3563f25bc1d93a91140bdd699a5fa39ea58408b5
kwaa <kwaa@users.noreply.github.com>
2020-08-14 19:21:43 +0000

    deploy: 928356711dceacadef9b3b84beab5d555d48d1cd

---
+++ sw.js
@@ -0,0 +1,25 @@
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.min.js');
+workbox.routing.registerRoute(
+    ({url}) => url.origin === 'https://kwaa.dev',
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'kwaadev',
+    })
+);
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'style' || request.destination === 'script',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'static',
+    })
+);
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst({
+        cacheName: 'image',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 30,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+            })
+        ],
+    })
+);
