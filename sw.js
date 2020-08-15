8261e4566fcfbb0e2666f9cb817d336889d73486
kwaa <kwaa@users.noreply.github.com>
2020-08-15 04:06:09 +0000

    deploy: c22b9142a8a4fd7c3ce79377a48ffd0820377dad

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst({
-        cacheName: 'image',
+        cacheName: 'images',
         plugins: [
             new workbox.expiration.ExpirationPlugin({
                 maxEntries: 30,
