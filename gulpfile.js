cec3aa9504f2f3ee732d8eb568a3ce90cd9f0baf
뤗 <50108258+0xb917@users.noreply.github.com>
2020-08-23 06:09:23 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ let gulp = require('gulp');
 let minifyInline = require('gulp-minify-inline');
 let htmlmin = require('gulp-htmlmin');
 let htmlclean = require('gulp-htmlclean');
-let imagemin = require('gulp-imagemin');
 
 // html
 gulp.task('minify-html', function() {
@@ -23,24 +22,6 @@ gulp.task('minify-inline', function() {
         .src('./public/**/*.html')
         .pipe(minifyInline())
         .pipe(gulp.dest('./public'))
-});
-// img
-gulp.task('minify-img', function() {
-    return gulp
-        .src('./public/p/**/*.*')
-        .pipe(
-            imagemin([
-                imagemin.gifsicle({ optimizationLevel: 3 }),
-                imagemin.mozjpeg({ progressive: true }),
-                imagemin.optipng({ optimizationLevel: 7 }),
-                imagemin.svgo({ plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]})],
-                { verbose: true }
-            )
-        )
-        .pipe(gulp.dest('./public/p'))
 });
 // task
 gulp.task('default', gulp.series(
