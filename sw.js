04ca6195c5abb69cbcf9b2467436fca4e2136a14
kwaa <kwaa@users.noreply.github.com>
2020-08-22 21:58:04 +0000

    deploy: ebace184c6e61de89ad651199c215d62ba256be8

--- sw.js
+++ sw.js
@@ -1,25 +1,19 @@
 importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.min.js');
 workbox.setConfig({modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/'});
 workbox.routing.registerRoute(
-    ({url}) => url.origin === 'https://kwaa.dev',
-    new workbox.strategies.NetworkFirst({
-        cacheName: 'kwaadev',
-    })
-);
-workbox.routing.registerRoute(
     ({request}) => request.destination === 'style' || request.destination === 'script',
     new workbox.strategies.StaleWhileRevalidate({
         cacheName: 'static',
     })
 );
 workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
+    /.*\.(?:png|jpg|jpeg|webp|svg|gif)/,
     new workbox.strategies.CacheFirst({
         cacheName: 'images',
         plugins: [
             new workbox.expiration.ExpirationPlugin({
-                maxEntries: 30,
-                maxAgeSeconds: 7 * 24 * 60 * 60,
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
             })
         ],
     })
