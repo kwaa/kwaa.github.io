254db25f56ff62d73c1f099cf80d2a83acd7039e
뤗 <50108258+0xb917@users.noreply.github.com>
2021-01-02 16:05:13 +0800

    update

--- themes/m/scripts/filter/after_post_render.js
+++ themes/m/scripts/filter/after_post_render.js
@@ -1,4 +1,4 @@
-hexo.extend.filter.register('after_post_render', function (data) {
+hexo.extend.filter.register('after_post_render', data => {
     data.content = data.content.replace(/<p>(<img [^>]*src="([^"]+)"[^>]*)><\/p>/g, '<figure class="mdui-img-fluid">' + '$1' + 'class="mdui-img-rounded" loading="lazy"></figure>')
     data.content = data.content.replace(/\bvideo-container\b/g, 'mdui-video-container')
     data.content = data.content.replace(/<table>/g, '<div class="mdui-table-fluid mdui-shadow-0"><table class="mdui-table mdui-table-hoverable">')
